086d59a412f1036223595121d85ed1e03fe54ca1
Jun Liang <51464205+jliangDS@users.noreply.github.com>
2020-06-27 22:35:55 -0500

    Create Vehicle2.java

---
+++ Vehicle2.java
@@ -0,0 +1,16 @@
+package com.cognixia.jump.corejava.classesandobjects;
+
+public class Vehicle2 {
+	
+	private String type;
+	private String color;
+	private int wheels;
+	
+	
+	@Override
+	public String toString() {
+		return "Vehicle2 [type=" + type + ", color=" + color + ", wheels=" + wheels + "]";
+	}
+	
+	
+}
