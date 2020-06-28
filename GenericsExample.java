c175ee36c4d19ea015d8b36bf8f6a239ab29b525
Jun Liang <51464205+jliangDS@users.noreply.github.com>
2020-06-27 22:36:54 -0500

    Create GenericsExample.java

---
+++ GenericsExample.java
@@ -0,0 +1,31 @@
+package com.cognixia.jump.corejava.classesandobjects;
+
+public class VehicleRunner {
+
+	public static void main(String[] args) {
+		
+//		Vehicle v = new Vehicle("car", "red", 4);
+		
+//		v.setType("truck");
+//		System.out.println(v.getType());
+//		
+//		v.describe();
+//		
+//		System.out.println(v);
+		
+//		Vehicle2 v2 = new Vehicle2("car", "red", 4);
+		
+//		System.out.println(v2);
+		
+//		v2.describe();
+		
+		
+		
+//		v.type = "car";
+//		v.color = "red";
+//		
+//		System.out.println(v.type);
+		
+	}
+
+}
